9de2444dac952c72da2f0923a05fd3abece33931
Benjamin De-graft-Andoh <degraftandohbenjamin@gmail.com>
2023-05-25 23:32:36 +0000

    JavaScript Assignment

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,22 @@
+let myName = "Benjamin";
+
+
+console.log(typeof myName);
+
+
+
+myName = "De-graft-Andoh";
+
+
+let car = {
+    brand: "Range Rover",
+    suspensionType: "Independent",
+    brakingSystem: "Disc brakes",
+    safetyFeatures: "Airbags",
+    bodyType: "SUV",
+    engineType: "Electric",
+    colour: "Black",
+    year: "2022",
+    ismanual: False,
+    ownerName: "Benjamin De-graft-Andoh" 
+}
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,21 @@
+
+const favouriteNumber = 12;
+
+
+const halfOfFavNum = favouriteNumber / 2;
+
+
+let isaacsFavorite = 13;
+
+
+isaacsFavorite = isaacsFavorite - favouriteNumber;
+
+
+
+isaacsFavorite = 26 * isaacsFavorite;
+
+
+
+isaacsFavorite = isaacsFavorite % 3;
+
+
